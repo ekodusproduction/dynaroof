d4b7f00b145471335bcdea89d6b453ac6957a69e
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-02-16 15:57:57 +0530

    verification complete

--- src/App.jsx
+++ src/App.jsx
@@ -59,78 +59,90 @@ function App() {
   const submitRegistrationForm = async (event) =>{
     event.preventDefault();
 
-    let formData = new FormData();
-    formData.append('fullName', event.target.fullName.value);
-    formData.append('email', event.target.email.value);
-    formData.append('phone', event.target.phoneNumber.value);
-    formData.append('dealerName', event.target.dealerName.value);
-    formData.append('materialType', event.target.materialType.value);
-    formData.append('dateOfPurchase', event.target.dateOfPurchase.value);
-    formData.append('country', event.target.country.value);
-    formData.append('district', event.target.district.value);
-    formData.append('state', event.target.state.value);
-    formData.append('colorOfSheets', event.target.colorOfSheets.value);
-    formData.append('numberOfSheets', event.target.numberOfSheets.value);
-    formData.append('serialNumber', event.target.serialNumber.value);
-    formData.append('thicknessOfSheets', event.target.thicknessOfSheets.value);
-    formData.append('invoice', event.target.invoice.files[0]);
-    setPhone(event.target.phoneNumber.value);
+    const terms_and_conditions = document.getElementById('checkTerms')
+    if(terms_and_conditions.checked){
 
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/api/customer-registration', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
+      let formData = new FormData();
+      formData.append('fullName', event.target.fullName.value);
+      formData.append('email', event.target.email.value);
+      formData.append('phone', event.target.phoneNumber.value);
+      formData.append('dealerName', event.target.dealerName.value);
+      formData.append('materialType', event.target.materialType.value);
+      formData.append('dateOfPurchase', event.target.dateOfPurchase.value);
+      formData.append('country', event.target.country.value);
+      formData.append('district', event.target.district.value);
+      formData.append('state', event.target.state.value);
+      formData.append('colorOfSheets', event.target.colorOfSheets.value);
+      formData.append('numberOfSheets', event.target.numberOfSheets.value);
+      formData.append('serialNumber', event.target.serialNumber.value);
+      formData.append('thicknessOfSheets', event.target.thicknessOfSheets.value);
+      formData.append('invoice', event.target.invoice.files[0]);
+      setPhone(event.target.phoneNumber.value);
 
-      if(response.data.status == 200 || response.data.type == 'success'){
-        const inputs = document.querySelectorAll('.otp-inputs');
-
-        inputs.forEach(function(input, index) {
-            input.addEventListener('keyup', function() {
-                if (this.value.length >= 1) {
-                    if (index < inputs.length - 1) {
-                        inputs[index + 1].focus();
-                    }
-                }
-            });
-    
-            input.addEventListener('keydown', function(e) {
-                if (e.key === 'Backspace' && this.value.length === 0) {
-                    if (index > 0) {
-                        inputs[index - 1].focus();
-                    }
-                }
-            });
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/customer-registration', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
         });
 
-        
-        document.getElementById("otp-model").classList.remove("d-none");
-        let count = 120;
-        const timer = setInterval(function() {
-          count--;
-          setTimerCount(count);
-          if (count === 0) {
-            clearInterval(timer);
-            document.getElementById('otpTimer').classList.add('d-none');
-            document.getElementById('resendOTP').classList.remove('d-none');
-          }
-        }, 1000);
-        document.getElementById('otpVerifyForm').reset();
-      }else{
+        if(response.data.status == 200 || response.data.type == 'success'){
+          const inputs = document.querySelectorAll('.otp-inputs');
+
+          inputs.forEach(function(input, index) {
+              input.addEventListener('keyup', function() {
+                  if (this.value.length >= 1) {
+                      if (index < inputs.length - 1) {
+                          inputs[index + 1].focus();
+                      }
+                  }
+              });
+      
+              input.addEventListener('keydown', function(e) {
+                  if (e.key === 'Backspace' && this.value.length === 0) {
+                      if (index > 0) {
+                          inputs[index - 1].focus();
+                      }
+                  }
+              });
+          });
+
+
+          document.getElementById("otp-model").classList.remove("d-none");
+          let count = 120;
+          const timer = setInterval(function() {
+            count--;
+            setTimerCount(count);
+            if (count === 0) {
+              clearInterval(timer);
+              document.getElementById('otpTimer').classList.add('d-none');
+              document.getElementById('resendOTP').classList.remove('d-none');
+            }
+          }, 1000);
+          document.getElementById('otpVerifyForm').reset();
+        }else{
+          Swal.fire({
+            title: 'Oops! Something went wrong',
+            text: response.data.message,
+            icon: 'error',
+          });
+        }
+      } catch (error) {
         Swal.fire({
           title: 'Oops! Something went wrong',
-          text: response.data.message,
+          text: error,
           icon: 'error',
         });
       }
-    } catch (error) {
+    }else{
       Swal.fire({
-        title: 'Oops! Something went wrong',
-        text: error,
-        icon: 'error',
-      });
+        title:'Oops!',
+        text:'Please accept Terms and Conditions',
+        icon:'warning'
+      })
     }
+
+    
   }
 
   const verifyOTPForm = async (event) => {
@@ -244,27 +256,27 @@ function App() {
       <div className='warranty-form-div'>
         <h4>Customer Warranty Registration Form</h4>
         <form id="registrationForm" onSubmit={submitRegistrationForm}>
-          <input type='text' name='fullName' className='form-control' placeholder='Full Name' />
-          <input type='email' name='email' className='form-control' placeholder='Email Address' />
-          <input type='text' name='phoneNumber' className='form-control' placeholder='Phone Number' />
+          <input type='text' name='fullName' className='form-control' placeholder='Full Name'  required/>
+          <input type='email' name='email' className='form-control' placeholder='Email Address'  required/>
+          <input type='text' name='phoneNumber' className='form-control' placeholder='Phone Number'  required/>
           
           <div className='row'>
             <div className='col-lg-4 col-md-6 col-sm-6'>
-              <input type='text' className='form-control' name='dealerName' placeholder='Dealer Name' />
+              <input type='text' className='form-control' name='dealerName' placeholder='Dealer Name'  required/>
             </div>
             <div className='col-lg-4 col-md-6 col-sm-6'>
-              <select name='materialType' className='form-control'>
+              <select name='materialType' className='form-control' required>
                 <option value='' disabled selected hidden>Select Material Type</option>
                 <option value='Super Pro (20 Years)'>Super Pro (20 Years)</option>
                 <option value='Pro (10 Years)'>Pro (10 Years)</option>
               </select>
             </div>
             <div className='col-lg-4 col-md-6 col-sm-6'>
-              <input type='date' className='form-control'name="dateOfPurchase"  placeholder='Date of Purchase :' />
+              <input type='date' className='form-control'name="dateOfPurchase"  placeholder='Date of Purchase :'  required/>
             </div>
           
             <div className='col-lg-4 col-md-6 col-sm-6'>
-              <select value={getSelectedCountry} name='country' className='form-control' onChange={getCountry}>
+              <select value={getSelectedCountry} name='country' className='form-control' onChange={getCountry} required>
                 <option value='' disabled selected hidden>Select Country</option>
                 <option value='india'>India</option>
                 <option value='bhutan'>Bhutan</option>
@@ -273,7 +285,7 @@ function App() {
             {
               getSelectedCountry == 'bhutan' ? 
               <div className='col-lg-8 col-md-6 col-sm-6'>
-                <select name='district' className='form-control'>
+                <select name='district' className='form-control' required>
                   <option value='' disabled selected hidden>Select District</option>
                   {
                     allDistricts.length > 0 ?
@@ -288,7 +300,7 @@ function App() {
               <>
 
                 <div className='col-lg-4 col-md-6 col-sm-6'>
-                  <select name='state' className='form-control' onChange={getSelectedState}>
+                  <select name='state' className='form-control' onChange={getSelectedState} required>
                     <option value='' disabled selected hidden>Select State</option>
                     {
                       allStates.length > 0 ?
@@ -300,7 +312,7 @@ function App() {
                   </select>
                 </div>
                 <div className='col-lg-4 col-md-6 col-sm-6'>
-                  <select name='district' className='form-control' onChange={getDistrictsOfStates}>
+                  <select name='district' className='form-control' onChange={getDistrictsOfStates} required>
 
                     <option value='' disabled selected hidden>Select District</option>
                     {
@@ -320,21 +332,21 @@ function App() {
           </div>
           <div className='row'>
             <div className='col-md-6'>
-              <input type='text' className='form-control' name='colorOfSheets' placeholder='Color of Sheets' /> 
+              <input type='text' className='form-control' name='colorOfSheets' placeholder='Color of Sheets'  required/> 
             </div>
             <div className='col-md-6'>
-              <input type='number' className='form-control' name='numberOfSheets' placeholder='Number of Sheets' min={0}/>
+              <input type='number' className='form-control' name='numberOfSheets' placeholder='Number of Sheets' min={0} required/>
             </div>
           </div>
           <div className='row'>
             <div className='col-md-6'>
               <div className="input-group">
-                <input type='text' className='form-control' name='serialNumber' placeholder='Serial Number' />
+                <input type='text' className='form-control' name='serialNumber' placeholder='Serial Number'  required/>
                 <span className="input-group-text"><IoMdInformationCircle /></span>
               </div>
             </div>
             <div className='col-md-6'>
-              <input type='number' className='form-control' name='thicknessOfSheets' placeholder='Thickness of Sheets (mm)' min={0}/>
+              <input type='number' className='form-control' name='thicknessOfSheets' placeholder='Thickness of Sheets (mm)' min={0} required/>
             </div>
           </div>
           <div className='upload-div'>
@@ -342,10 +354,10 @@ function App() {
               <h6>Upload Invoice</h6>
               <p>( Accepted file types: pdf, docx. Max file size: 2MB)</p>
             </div>
-            <input type='file' name='invoice' className='form-control' />
+            <input type='file' name='invoice' className='form-control'  required/>
           </div>
           <div className='terms-condition'>
-            <input type='checkbox' name='terms-condition' />
+            <input type='checkbox' name='terms-condition' id="checkTerms" />
             <p>I accept <a href='#'>Terms & Conditions</a></p>
           </div>
           <div className='submit-button'>
@@ -367,12 +379,12 @@ function App() {
                 <input type='hidden' name='phone' className='form-control' placeholder='Email Address' value={getPhone} />
               </div>
               <div className="form-group mb-4 d-flex flex-row justify-content-evenly align-items-center otp-input-box">
-                <input type='text' name='otp1' className='form-control otp-inputs' maxLength="1" />
-                <input type='text' name='otp2' className='form-control otp-inputs' maxLength="1" />
-                <input type='text' name='otp3' className='form-control otp-inputs' maxLength="1" />
-                <input type='text' name='otp4' className='form-control otp-inputs' maxLength="1" />
-                <input type='text' name='otp5' className='form-control otp-inputs' maxLength="1" />
-                <input type='text' name='otp6' className='form-control otp-inputs' maxLength="1" />
+                <input type='text' name='otp1' className='form-control otp-inputs' maxLength="1" required/>
+                <input type='text' name='otp2' className='form-control otp-inputs' maxLength="1" required/>
+                <input type='text' name='otp3' className='form-control otp-inputs' maxLength="1" required/>
+                <input type='text' name='otp4' className='form-control otp-inputs' maxLength="1" required/>
+                <input type='text' name='otp5' className='form-control otp-inputs' maxLength="1" required/>
+                <input type='text' name='otp6' className='form-control otp-inputs' maxLength="1" required/>
               </div>
               <div className='submit-button d-flex flex-row justify-content-between align-items-center'>
                 <input type='submit' className='btn btn-md btn-success' name='Submit' value='Verify' />
