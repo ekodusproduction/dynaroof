2c08f3ae80cb76915506a8406537709aad059935
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-03-05 17:34:58 +0530

    new product image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
   const [getTimerCount, setTimerCount] = useState(120);
 
   // const base_url = 'http://127.0.0.1:8000';
-  const base_url = 'https://dynaroofwarrantybackend.ekodusproject.tech';
+  const base_url = 'https://wa-backend.dynaroof.com';
   
 
   const getCountry = (event) => {
